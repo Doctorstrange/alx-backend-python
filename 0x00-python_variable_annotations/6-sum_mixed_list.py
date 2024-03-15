be293d9c5027025e9f503f5ba303ff3ced9d7485
Doctorstrange <doctorstrange247@gmail.com>
2024-03-15 02:12:13 +0000

    6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Returns the string representation of the given float `n`."""
-from typing import Union
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
-    """Returns the sum of the integers and floats"""
-
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Returns:
+        float: The sum of the elements in the list.
+    """
     return sum(mxd_lst)
