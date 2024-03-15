aae1e57d689c96775d2c45c2ab83bbb0569b1bc3
Doctorstrange <doctorstrange247@gmail.com>
2024-03-15 01:33:19 +0000

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Returns the string representation of the given float `n`."""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of the given float `n`."""
+
+    return str(n)
