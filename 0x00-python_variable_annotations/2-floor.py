a1b70bf30f0d1e350bf8685d2c0d0ed2f819f464
Doctorstrange <doctorstrange247@gmail.com>
2024-03-15 01:24:45 +0000

    2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""float: The sum of the two numbers."""
+
+
+import math
+
+
+def floor(n: float) -> float:
+    """
+    Returns:
+        float: The floor of the input float.
+    """
+    return math.floor(n)
