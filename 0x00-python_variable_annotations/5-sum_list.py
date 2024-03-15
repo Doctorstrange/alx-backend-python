bc55b9f14e7b9261c526d7f9a6659b224b3e8f26
Doctorstrange <doctorstrange247@gmail.com>
2024-03-15 01:40:16 +0000

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Returns the string representation of the given float `n`."""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns the sum of the floats in the given list"""
+
+    return sum(input_list)
