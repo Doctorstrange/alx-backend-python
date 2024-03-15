c1693f519e62490165e6dc4ecd5a761b45a7e8d1
Doctorstrange <doctorstrange247@gmail.com>
2024-03-15 01:37:39 +0000

    4-define_variables.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Returns the string representation of the given float `n`."""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
