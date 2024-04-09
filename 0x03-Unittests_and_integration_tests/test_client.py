cc13c5ab7ea4b49b981e4ba8b0682f16266454a3
doctorstrange <doctorstrange@gmail.com>
2024-04-09 02:13:37 +0000

    5. Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -20,16 +20,11 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(f'https://api.github.com/orgs/{org_name}')
 
     def test_public_repos_url(self):
-        # Define a known payload
         mock_payload = {"repos_url": "https://api.github.com/orgs/test_org/repos"}
 
-        # Patch the org property to return the known payload
         with patch.object(GithubOrgClient, 'org', return_value=mock_payload):
-            # Create an instance of GithubOrgClient
             test_client = GithubOrgClient('test_org')
 
-            # Call the _public_repos_url method
             result = test_client._public_repos_url()
 
-            # Assert that the result is the expected one
             self.assertEqual(result, "https://api.github.com/orgs/test_org/repos")
