003920ee6743deb317379d334e943bad16c9f40b
doctorstrange <doctorstrange@gmail.com>
2024-04-09 02:22:33 +0000

    7. Parameterize

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -45,3 +45,12 @@ class TestGithubOrgClient(unittest.TestCase):
 
         mock_public_repos_url.assert_called_once()
         mock_get_json.assert_called_once_with("https://api.github.com/orgs/test_org/repos")
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected_result):
+        test_client = GithubOrgClient('test_org')
+        result = test_client.has_license(license_key, [repo])
+        self.assertEqual(result, expected_result)
