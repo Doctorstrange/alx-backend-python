d029990fa5c9cc913cc3b53dcbf695ce2a8bf961
doctorstrange <doctorstrange@gmail.com>
2024-04-09 02:26:34 +0000

    8. Integration test: fixtures

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -57,30 +57,38 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, expected_result)
 
 class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """Performs integration tests for the `GithubOrgClient` class."""
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Sets up class fixtures before running tests."""
+        route_payload = {
+            'https://api.github.com/orgs/google': cls.org_payload,
+            'https://api.github.com/orgs/google/repos': cls.repos_payload,
+        }
+
+        def get_payload(url):
+            if url in route_payload:
+                return Mock(**{'json.return_value': route_payload[url]})
+            return HTTPError
+
+        cls.get_patcher = patch("requests.get", side_effect=get_payload)
+        cls.get_patcher.start()
+
+    def test_public_repos(self) -> None:
+        """Tests the `public_repos` method."""
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(),
+            self.expected_repos,
+        )
+
+    def test_public_repos_with_license(self) -> None:
+        """Tests the `public_repos` method with a license."""
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(license="apache-2.0"),
+            self.apache2_repos,
+        )
 
     @classmethod
-    def setUpClass(cls):
-        cls.get_patcher = patch('client.requests.get')
-        cls.mock_get = cls.get_patcher.start()
-
-        # Configure the side_effect of mock_get
-        cls.mock_get.side_effect = [
-            unittest.mock.Mock(json=lambda: cls.org_payload),
-            unittest.mock.Mock(json=lambda: cls.repos_payload)
-        ]
-
-    @classmethod
-    def tearDownClass(cls):
+    def tearDownClass(cls) -> None:
+        """Removes the class fixtures after running all tests."""
         cls.get_patcher.stop()
-
-    def test_public_repos(self):
-        test_client = GithubOrgClient('test_org')
-        repos = test_client.public_repos()
-
-        self.assertEqual(repos, self.expected_repos)
-
-    def test_public_repos_with_license(self):
-        test_client = GithubOrgClient('test_org')
-        apache2_repos = test_client.public_repos('Apache-2.0')
-
-        self.assertEqual(apache2_repos, self.apache2_repos)
